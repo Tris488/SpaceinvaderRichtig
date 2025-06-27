573be9877668009ce7e0b0aa4ae35ed03ede6074
tris488 <tristanbecker12@gmail.com>
2025-06-27 09:08:56 +0200

    extrem

--- powerup.h
+++ powerup.h
@@ -5,12 +5,16 @@
 #include <SDL3/SDL.h>
 #include <stdbool.h>
 #include "entity.h"
+#include "health.h"
 
 #define MAX_POWERUPS 10
 
 // Power-Up Typen
 typedef enum {
     POWERUP_DOUBLE_SHOOT,
+    POWERUP_HEART,
+    POWERUP_SPEED,
+    POWERUP_BOMB,
     POWERUP_TYPE_COUNT
 } PowerupType;
 
@@ -26,25 +30,29 @@ typedef struct {
     float duration;
     float effect_strength;
 } Powerup;
+typedef struct {
+    PowerupType type;
+    SDL_FRect sprite_rect;
+    float duration;
+    float effect_strength;
+    float fall_speed;
+    float spawn_chance;
+} PowerupDefinition;
 
-// Power-Up System initialisieren
+
 Entity init_powerup_system(SDL_Renderer* renderer);
-
-// Power-Up spawnen
 void powerup_spawn(float x, float y);
-
-// Kollision pruefen
+void powerup_spawn_specific(float x, float y, PowerupType type);
 Powerup* powerup_check_collision(SDL_FRect* player_rect);
-
-// Alle Power-Ups zuruecksetzen
 void powerup_reset_all(void);
-
-// Double Shoot Status
 bool is_double_shoot_active(void);
 float get_double_shoot_multiplier(void);
-
-// Wave-Callback fuer Power-Up Spawning
 void powerup_on_wave_start(int wave_number);
 void powerup_on_enemy_killed(float x, float y);
+float get_speed_multplier(void);
+bool is_speed_active();
+void bomb_got_used();
+
+
 
 #endif // POWERUP_H
